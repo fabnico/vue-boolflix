7643993a4265695e0d45d43592b7fd0173663c00
Fabrizio <whoet.wo@gmail.com>
2020-12-03 17:15:52 +0100

    Milestone 2 completata

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const app = new Vue({
    data: {
       myAPI: '18d96878a8e77682ff38f707ebc18bb0',
       films: [],
+      tvSeries: [],
       userInput: '',
-
    },
    methods:{
       search: function(){
-         let a = axios.get('https://api.themoviedb.org/3/search/movie',
+         axios.get('https://api.themoviedb.org/3/search/movie',
       {
          params: {
             api_key: this.myAPI,
@@ -22,7 +22,24 @@ const app = new Vue({
          this.films.forEach(e => {e.vote_average = Math.ceil(e.vote_average / 2);
          e.star = 5 - e.vote_average;
             })
+         }),
+         axios.get('https://api.themoviedb.org/3/search/tv',
+      {
+         params: {
+            api_key: this.myAPI,
+            query: this.userInput
+         }
+      })
+         .then(result => {
+         this.tvSeries = result.data.results;
+         this.tvSeries.forEach(e => {e.vote_average = Math.ceil(e.vote_average / 2);
+         e.star = 5 - e.vote_average;
+            })
          })
-      }
+      },
+      flagError: function(i){
+         this.films[i].original_language = 'flag_404';
+         this.tvSeries[i].original_language = 'flag_404';
+      },
    }
 })
